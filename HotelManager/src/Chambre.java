06b54d6adca8694f129b568d77165d327c54ec6d
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2022-03-17 13:01:24 +0100

    Ajout de méthodes pour Chambre

--- HotelManager/src/Chambre.java
+++ HotelManager/src/Chambre.java
@@ -2,13 +2,43 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public abstract class Chambre {
-    private int id;
+    private int numero;
     private int etage;
     private double prix;
 
     private ArrayList<Reservation> reservations;
+    private ArrayList<Sejour> sejours;
 
-    boolean disponible(Date debut, Date fin) {
+    public Chambre(int num, int e, double p) {
+        numero = num;
+        etage = e;
+        prix = p;
+        reservations = new ArrayList<>();
+        sejours = new ArrayList<>();
+    }
+
+    public boolean disponible(Date debut, Date fin) {
+        for (Reservation r : reservations) {
+            if ((fin.after(r.getDebut()) && fin.before(r.getFin())) || (debut.after(r.getDebut()) && debut.before(r.getFin())))
+                return false;
+        }
+
         return true;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setPrix(double p) {
+        prix = p;
+    }
+
+    public void ajouterReservation(Reservation reservation) {
+        reservations.add(reservation);
+    }
+
+    public double getPrix() {
+        return prix;
     }
 }
