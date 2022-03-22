751afdfbb7c91c1c372c2424678197babe7eb19a
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2022-03-23 00:57:20 +0100

    Suppression de la classe Main Ajout d'une méthode main à la classe HotelManager

--- HotelManager/src/HotelManager.java
+++ HotelManager/src/HotelManager.java
@@ -1,2 +1,8 @@
+import view.HotelView;
+
 public class HotelManager {
+
+    public static void main(String[] args) {
+        new HotelView();
+    }
 }
--- HotelManager/src/Main.java
+++
@@ -1,7 +0,0 @@
-import view.HotelView;
-
-public class Main {
-    public static void main(String[] args) {
-        new HotelView();
-    }
-}
