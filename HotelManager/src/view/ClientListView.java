8a69bd5d5388e3431f287095dd8b234863c5c137
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2022-03-24 00:49:08 +0100

    Ajout d'un suffixe List pour les vues et les contrôleurs dont le modèle est une liste

---
+++ HotelManager/src/view/ClientListView.java
@@ -0,0 +1,15 @@
+package view;
+
+import controller.ClientListController;
+
+import javax.swing.*;
+
+public class ClientListView extends JPanel {
+    public ClientListView() {
+
+    }
+
+    public void setupEvents(ClientListController controller) {
+
+    }
+}
