744cc56331e003b0f2dd4d27803501ea3313c008
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2022-03-24 11:48:33 +0100

    Ajout du système d'ajout de Chambre dans le controller Changement du model de ArrayList<Chambre> à Hotel

--- HotelManager/src/controller/ChambreListController.java
+++ HotelManager/src/controller/ChambreListController.java
@@ -1,8 +1,6 @@
 package controller;
 
-import model.Chambre;
-import model.ChambreSimple;
-import model.ChambreType;
+import model.*;
 import view.ChambreListView;
 
 import java.util.ArrayList;
@@ -11,20 +9,31 @@ import java.util.Random;
 public class ChambreListController {
     private static int id = 2;
 
-    private ArrayList<Chambre> model;
+    private Hotel model;
     private ChambreListView view;
 
-    public ChambreListController(ArrayList<Chambre> model, ChambreListView view) {
+    public ChambreListController(Hotel model, ChambreListView view) {
         this.model = model;
         this.view = view;
 
         view.setupEvents(this);
-        view.modelUpdated(model);
+        view.modelUpdated(model.getChambres());
     }
 
     public void addChambre(int numero, int etage, double prix, ChambreType type) {
-        model.add(new ChambreSimple(null, id++, etage, Math.floor((Math.random() + 1.0) * 60)));
+        switch (type) {
+            case CHAMBRE_SIMPLE -> model.ajouterChambre(new ChambreSimple(model, numero, etage, prix));
+            case CHAMBRE_DOUBLE -> model.ajouterChambre(new ChambreDouble(model, numero, etage, prix));
+            case SUITE_SIMPLE -> model.ajouterChambre(new SuiteSimple(model, numero, etage, prix));
+            case SUITE_PRESIDENTIELLE -> model.ajouterChambre(new SuitePresidentielle(model, numero, etage, prix));
+        }
 
-        view.modelUpdated(model);
+        view.modelUpdated(model.getChambres());
+    }
+
+    public void deleteChambre(int index) {
+        model.getChambres().remove(index);
+
+        view.modelUpdated(model.getChambres());
     }
 }
